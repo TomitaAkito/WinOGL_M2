4b851fdc54c9aad77898f0f2c02ec76fbee9353c
TomitaAkito <145421759+TomitaAkito@users.noreply.github.com>
2026-07-12 03:04:30 +0900

    プロジェクトファイルを追加。

---
+++ WinOGL/WinOGLView.cpp
@@ -0,0 +1,151 @@
+﻿
+// WinOGLView.cpp : CWinOGLView クラスの実装
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS は、プレビュー、縮小版、および検索フィルター ハンドラーを実装している ATL プロジェクトで定義でき、
+// そのプロジェクトとのドキュメント コードの共有を可能にします。
+#ifndef SHARED_HANDLERS
+#include "WinOGL.h"
+#endif
+
+#include "WinOGLDoc.h"
+#include "WinOGLView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CWinOGLView
+
+IMPLEMENT_DYNCREATE(CWinOGLView, CView)
+
+BEGIN_MESSAGE_MAP(CWinOGLView, CView)
+//	ON_WM_LBUTTONDBLCLK()
+ON_WM_LBUTTONDOWN()
+ON_WM_CREATE()
+ON_WM_DESTROY()
+ON_WM_ERASEBKGND()
+END_MESSAGE_MAP()
+
+// CWinOGLView コンストラクション/デストラクション
+
+CWinOGLView::CWinOGLView() noexcept
+{
+	// TODO: 構築コードをここに追加します。
+
+}
+
+CWinOGLView::~CWinOGLView()
+{
+}
+
+BOOL CWinOGLView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: この位置で CREATESTRUCT cs を修正して Window クラスまたはスタイルを
+	//  修正してください。
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CWinOGLView 描画
+
+void CWinOGLView::OnDraw(CDC* pDC) {
+	CWinOGLDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if(!pDoc)
+		return;
+
+	wglMakeCurrent(pDC->m_hDC, m_hRC);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT /* | GL_DEPTH_BUFFER_BIT*/);
+
+	glColor3f(1.0, 1.0, 1.0); // 色を設定
+	glBegin(GL_LINE_STRIP); // 描画の開始
+	glVertex2f(-1.0, 0.5); // 1点目の座標
+	glVertex2f(0.0, -0.5); // 2点目の座標
+	glVertex2f(1.0, 0.5); // 3点目の座標
+	glVertex2f(-1.0, 0.5); // 4点目の座標(始点に戻る)
+	glEnd(); // 描画の終了
+
+	glFlush();
+	SwapBuffers(pDC->m_hDC);
+	wglMakeCurrent(pDC->m_hDC, NULL);
+}
+
+
+// CWinOGLView の診断
+
+#ifdef _DEBUG
+void CWinOGLView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CWinOGLView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CWinOGLDoc* CWinOGLView::GetDocument() const // デバッグ以外のバージョンはインラインです。
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CWinOGLDoc)));
+	return (CWinOGLDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CWinOGLView メッセージ ハンドラー
+
+//void CWinOGLView::OnLButtonDblClk(UINT nFlags, CPoint point) {
+	// TODO: ここにメッセージ ハンドラー コードを追加するか、既定の処理を呼び出します。
+
+//	CView::OnLButtonDblClk(nFlags, point);
+//}
+
+void CWinOGLView::OnLButtonDown(UINT nFlags, CPoint point) {
+	// TODO: ここにメッセージ ハンドラー コードを追加するか、既定の処理を呼び出します。
+
+	CView::OnLButtonDown(nFlags, point);
+}
+
+int CWinOGLView::OnCreate(LPCREATESTRUCT lpCreateStruct) {
+	if(CView::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	PIXELFORMATDESCRIPTOR pfd =
+	{
+	sizeof(PIXELFORMATDESCRIPTOR),
+	1,
+	PFD_DRAW_TO_WINDOW |
+	PFD_SUPPORT_OPENGL |
+	PFD_DOUBLEBUFFER,
+	PFD_TYPE_RGBA,
+	32,
+	0,0,0,0,0,0,
+	0,0,0,0,0,0,0,
+	24,
+	0,0,
+	PFD_MAIN_PLANE,
+	0,
+	0,0,0
+	};
+	CClientDC clientDC(this);
+	int pixelFormat = ChoosePixelFormat(clientDC.m_hDC,
+										&pfd);
+	SetPixelFormat(clientDC.m_hDC, pixelFormat, &pfd);
+	m_hRC = wglCreateContext(clientDC.m_hDC);
+
+	return 0;
+}
+
+void CWinOGLView::OnDestroy() {
+	CView::OnDestroy();
+	wglDeleteContext(m_hRC);
+}
+
+BOOL CWinOGLView::OnEraseBkgnd(CDC* pDC) {
+	return true;
+}
